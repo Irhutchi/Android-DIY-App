25c67354cd30e7b2c02421d08c12dffbb736fe91
Ian <ianhutch90@gmail.com>
2021-10-23 12:56:25 +0100

    Add ImageCapture and Storage (locally)
    Source: https://developer.android.com/codelabs/camerax-getting-started#4

--- app/src/main/java/ie/wit/doityourself/activities/CameraActivity.kt
+++ app/src/main/java/ie/wit/doityourself/activities/CameraActivity.kt
@@ -1,21 +1,26 @@
 package ie.wit.doityourself.activities
 
 import android.content.pm.PackageManager
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.util.Log.i
 import android.widget.Toast
 import androidx.camera.core.CameraSelector
 import androidx.camera.core.ImageCapture
+import androidx.camera.core.ImageCaptureException
 import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import ie.wit.doityourself.R
 import ie.wit.doityourself.databinding.ActivityCameraBinding
-import ie.wit.doityourself.databinding.ActivityDiyBinding
 import ie.wit.doityourself.main.MainApp
+import java.io.File
 import java.lang.Exception
+import java.text.SimpleDateFormat
+import java.util.*
 
 class CameraActivity : AppCompatActivity() {
 
@@ -24,6 +29,7 @@ class CameraActivity : AppCompatActivity() {
     // objects on a particular layout
     private lateinit var binding: ActivityCameraBinding
     private var imageCapture:ImageCapture?=null
+    private lateinit var outputDirectory: File
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,6 +38,8 @@ class CameraActivity : AppCompatActivity() {
 
         // Retrieving and storing a reference to the MainApp object
         app = this.application as MainApp
+
+        outputDirectory = getOutputDirectory()
 
         if (allPermissionGranted()) {
             startCamera()
@@ -42,6 +50,59 @@ class CameraActivity : AppCompatActivity() {
                 Constants.REQUEST_CODE_PERMISSIONS
             )
         }
+
+        binding.btnTakePhoto.setOnClickListener {
+            takePhoto()
+        }
+    }
+
+    private fun getOutputDirectory(): File {
+        val mediaDir = externalMediaDirs.firstOrNull()?.let { mFile ->
+            File(mFile, resources.getString(R.string.app_name)).apply {
+                mkdirs()
+            }
+        }
+        return if (mediaDir != null && mediaDir.exists())
+            mediaDir else filesDir
+    }
+
+    private fun takePhoto() {
+        // Get a stable reference of the modifiable image capture use case
+        val imageCapture = imageCapture?: return
+        // Create time-stamped output file to hold the image
+        val photoFile = File(
+            outputDirectory, SimpleDateFormat(Constants.FILE_NAME_FORMAT,
+                   Locale.getDefault()).format(System.currentTimeMillis()) + ".jpg")
+        // Create output options object which contains file + metadata
+        val outputOption = ImageCapture
+            .OutputFileOptions
+            .Builder(photoFile)
+            .build()
+        // Set up image capture listener, which is triggered after photo has been taken
+        imageCapture.takePicture(
+            outputOption, ContextCompat.getMainExecutor(this),
+            object : ImageCapture.OnImageSavedCallback{
+                override fun onImageSaved(outputFileResults: ImageCapture.OutputFileResults) {
+
+                    val savedUri = Uri.fromFile(photoFile)
+                    val msg = "Photo Saved"
+
+                    Toast.makeText(
+                        this@CameraActivity,
+                        "$msg $savedUri",
+                        Toast.LENGTH_LONG
+                    ).show()
+
+                }
+
+                override fun onError(exception: ImageCaptureException) {
+                    Log.e(Constants.TAG,
+                        "onError: ${exception.message}",
+                        exception)
+                }
+            }
+        )
+
     }
 
     private fun startCamera() {
@@ -50,7 +111,9 @@ class CameraActivity : AppCompatActivity() {
             .getInstance(this)
 
         cameraProviderFuture.addListener({
+            // Used to bind the lifecycle of cameras to the lifecycle owner
             val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+                // Preview
             val preview = Preview.Builder()
                 .build()
                 .also { mPreview ->
@@ -61,18 +124,20 @@ class CameraActivity : AppCompatActivity() {
                 }
             imageCapture = ImageCapture.Builder()
                 .build()
+            // Select back camera as a default
             val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
 
             try {
+                // Unbind use cases before rebinding
                 cameraProvider.unbindAll()
-
+                // Bind use cases to camera
                 cameraProvider.bindToLifecycle (
                     this, cameraSelector,
                     preview, imageCapture
                 )
 
             } catch (err: Exception) {
-                Log.d(Constants.TAG, "startCamera Fail:", err)
+                i(Constants.TAG, "startCamera Fail:", err)
             }
         }, ContextCompat.getMainExecutor(this))
     }
@@ -81,7 +146,7 @@ class CameraActivity : AppCompatActivity() {
         requestCode: Int,
         permissions: Array<String>,
         grantResults: IntArray) {
-        //super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
 
         if (requestCode == Constants.REQUEST_CODE_PERMISSIONS) {
             if (allPermissionGranted()){
